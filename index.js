231db6726d7ab4fc902467a1e5e2e45818611dd6
maksimramazanov <maksim.ramazanov@pipedrive.com>
2022-08-02 00:47:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa')
+const app = new Koa();
+
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
+
+const mainRouter = require('./lib/routes/main-router');
+app.use(mainRouter.routes());
+
+app.listen(3000, () => {
+    console.log(`Service is running on http://localhost:3000`);
+  });
